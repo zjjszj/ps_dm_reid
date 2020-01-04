572611408ab9110247e02ed91bd5bdacf5836913
zhangjin <1411605960+zjjszj@users.noreply.github.com>
2020-01-04 10:33:24 +0800

    worker=4 & epoch=200

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class DefaultConfig(object):
     # optimization options
     loss = 'triplet'
     optim = 'adam'
-    max_epoch = 60      #400
+    max_epoch = 200      #400
     train_batch = 32 
     test_batch = 32
     adjust_lr = False
