876655a3c69747dcaef48f27b339dc462b85ee2e
zhangjin <1411605960+zjjszj@users.noreply.github.com>
2020-01-04 17:41:53 +0800

    update averaging operation

--- models/networks.py
+++ models/networks.py
@@ -64,28 +64,29 @@ class BatchDrop(nn.Module):
     
     def forward(self, x):
         if self.training:
-            h, w = x.size()[-2:]
+        #     h, w = x.size()[-2:]
+        #     rh = round(self.h_ratio * h)
+        #     rw = round(self.w_ratio * w)
+        #     sx = random.randint(0, h-rh)
+        #     sy = random.randint(0, w-rw)
+        #     mask = x.new_ones(x.size())
+        #     mask[:, :, sx:sx+rh, sy:sy+rw] = 0
+        #     x = x * mask
+        # #print('x.shape====',x.shape)        [32, 2048, 24, 8]
+        # print(x)
+
+            #用均值代替
+            c,h, w = x.size()[-3:]
             rh = round(self.h_ratio * h)
             rw = round(self.w_ratio * w)
             sx = random.randint(0, h-rh)
             sy = random.randint(0, w-rw)
-            mask = x.new_ones(x.size())
-            mask[:, :, sx:sx+rh, sy:sy+rw] = 0
-            x = x * mask
-        #print('x.shape====',x.shape)        [32, 2048, 24, 8]
-        print(x)
-
-            #用均值代替
-            # c,h, w = x.size()[-3:]
-            # rh = round(self.h_ratio * h)
-            # rw = round(self.w_ratio * w)
-            # sx = random.randint(0, h-rh)
-            # sy = random.randint(0, w-rw)
-            # for i in range(c):
-            #     scope=x[:,i,sx:sx+rh, sy:sy+rw]
-            #     scope_aveg=torch.sum(scope)/(rh*rw)
-            #     x[:, i, sx:sx + rh, sy:sy + rw]=scope_aveg
-            # #print('x.shape===',x.shape)    [32, 2048, 24, 8]
+            for i in range(c):
+                scope=x[:,i,sx:sx+rh, sy:sy+rw]
+                scope_aveg=torch.sum(scope)/(rh*rw)
+                x[:, i, sx:sx + rh, sy:sy + rw]=scope_aveg
+            #print('x.shape===',x.shape)    [32, 2048, 24, 8]
+            print(x)
         return x
 
 class BatchCrop(nn.Module):
