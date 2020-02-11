7177b1c5d143fc1462fe9e5eba59d39f75ef3134
zhangjin <1411605960+zjjszj@users.noreply.github.com>
2020-02-11 17:27:46 +0800

    add and use oim

--- my_test.py
+++ my_test.py
@@ -17,11 +17,8 @@ import random
 #     x[:, i, sx:sx + rh, sy:sy + rw]=scope_aveg
 #
 # print('x===',x)
-from torch.autograd import Variable
-a=torch.tensor([[1,2],[2,2.0]]).cuda()
-b=torch.tensor([[1,2],[2,2.0]]).cuda()
-c=b.mm(a.t())
-print(c)
+a=[2]
+print(a[0])
 
 
 
