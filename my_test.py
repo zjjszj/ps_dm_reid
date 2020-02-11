a59fce044c66922f5ea3fefb553b3195cfb6cb8c
zhangjin <1411605960+zjjszj@users.noreply.github.com>
2020-02-11 12:22:48 +0800

    add and use oim

--- my_test.py
+++ my_test.py
@@ -1,32 +1,25 @@
 import torch
 import random
 
-
 #读入图片
-x=torch.zeros((2,3,2,2))
-x[:,:,1:2,0:3]=1
-print(x)
-#用均值代替
-c,h, w = x.size()[-3:]
-rh = round(0.5 * h)
-rw = round(0.5* w)
-sx = random.randint(0, h-rh)
-sy = random.randint(0, w-rw)
-for i in range(c):
-    scope=x[:,i,sx:sx+rh, sy:sy+rw]
-    scope_aveg=torch.sum(scope)/(rh*rw)
-    x[:, i, sx:sx + rh, sy:sy + rw]=scope_aveg
+# x=torch.zeros((2,3,2,2))
+# x[:,:,1:2,0:3]=1
+# print(x)
+# #用均值代替
+# c,h, w = x.size()[-3:]
+# rh = round(0.5 * h)
+# rw = round(0.5* w)
+# sx = random.randint(0, h-rh)
+# sy = random.randint(0, w-rw)
+# for i in range(c):
+#     scope=x[:,i,sx:sx+rh, sy:sy+rw]
+#     scope_aveg=torch.sum(scope)/(rh*rw)
+#     x[:, i, sx:sx + rh, sy:sy + rw]=scope_aveg
+#
+# print('x===',x)
 
-print('x===',x)
-
-
-
-
-
-
-
-
-
+a=[1,2]
+print(sum(a))
 
 
 
--- utils/loss.py
+++ utils/loss.py
@@ -3,6 +3,7 @@ import random
 import torch
 from torch import nn
 import torch.nn.functional as F
+from torch import autograd
 
 def topk_mask(input, dim, K = 10, **kwargs):
     index = input.topk(max(1, min(K, input.size(dim))), dim = dim, **kwargs)[1]
@@ -122,6 +123,57 @@ class TripletLoss(object):
         else:
             loss = self.ranking_loss(dist_an - dist_ap, y)
         return loss, dist_ap, dist_an
+
+##oim loss
+class OIM(autograd.Function):
+    """myself
+    Modified from Tong Xiao's open-reid (https://github.com/Cysu/open-reid).
+    """
+    def __init__(self, lut, momentum=0.5):
+        super(OIM, self).__init__()
+        self.lut = lut
+        self.momentum = momentum
+
+    def forward(self, inputs, targets):
+        self.save_for_backward(inputs, targets)
+        outputs = inputs.mm(self.lut.t())
+        return outputs
+
+    def backward(self, grad_outputs):
+        inputs, targets = self.saved_tensors
+        grad_inputs = None
+        if self.needs_input_grad[0]:
+            grad_inputs = grad_outputs.mm(self.lut)
+        for x, y in zip(inputs, targets):
+            self.lut[y] = self.momentum * self.lut[y] + (1. - self.momentum) * x
+            self.lut[y] /= self.lut[y].norm()
+        return grad_inputs, None
+
+
+def oim(inputs, targets, lut, momentum=0.5):
+    return OIM(lut, momentum=momentum)(inputs, targets)
+
+
+class OIMLoss(nn.Module):
+    def __init__(self, num_features, num_classes, scalar=1.0, momentum=0.5,
+                 weight=None, size_average=True):
+        super(OIMLoss, self).__init__()
+        self.num_features = num_features
+        self.num_classes = num_classes
+        self.momentum = momentum
+        self.scalar = scalar
+        self.weight = weight
+        self.size_average = size_average
+
+        self.register_buffer('lut', torch.zeros(num_classes, num_features))
+
+    def forward(self, inputs, targets):
+        inputs = oim(inputs, targets, self.lut, momentum=self.momentum) #(N,d)、输出得分
+        inputs *= self.scalar
+        loss = F.cross_entropy(inputs, targets, weight=self.weight,
+                               size_average=self.size_average)
+        return loss, inputs
+
 
 
 class CrossEntropyLabelSmooth(nn.Module):
