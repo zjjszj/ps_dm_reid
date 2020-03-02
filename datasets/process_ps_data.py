f734cd52417880cff69b1f629e6817ebc7ed6b69
zhangjin <1411605960+zjjszj@users.noreply.github.com>
2020-03-02 14:48:24 +0800

    increase evaluate

--- datasets/process_ps_data.py
+++ datasets/process_ps_data.py
@@ -263,7 +263,7 @@ class ps_data_manager:
         save=False
         if load:
             # load
-            q_inputs=_load('q_inputs.pkl','/kaggle/input/q_inputs')
+            q_inputs=_load('q_inputs.pkl','/kaggle/input/q-inputs')
         else:
             #q_inputs=self.get_query_inputs()
             #print('begin...get_gallery_det...')
