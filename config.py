549054694b3ebe6a8988c3175922a30894debacb
zhangjin <1411605960+zjjszj@users.noreply.github.com>
2020-03-12 21:25:24 +0800

    use orignal network with 128d

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ class DefaultConfig(object):
     train_batch = 16
     nums_pedes=16
     test_batch = 32
-    adjust_lr = True
-    lr = 0.001
+    adjust_lr = False
+    lr = 0.0001
     gamma = 0.1
     weight_decay = 5e-4
     momentum = 0.9
